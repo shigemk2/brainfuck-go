c9e1c2b6685c54777b0fb3f4ff79d3c2f68bbcc5
shigemk2 <i.am.shige@gmail.com>
2014-10-17 21:28:04 +0900

    とりあえずループ部分以外は全部実装した

--- brainfuck.go
+++ brainfuck.go
@@ -6,39 +6,70 @@ import (
 )
 
 func main() {
-	input, err := os.Open("brainfuck/hoge.b")
+	input, err := os.Open("brainfuck/33.b")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	pc := 0
-	mem := make([]byte, 30000)
+	reg := 0
+	mem := make([]int, 30000)
 	buff := make([]byte, 30000)
 	for {
 		c, _ := input.Read(buff)
 		if c == 0 {
 			break
 		}
-		// str := string(buff[:c])
-		// // fmt.Print(str)
-		// if str == "-" {
-		// 	fmt.Print(str)
-		// }
-		switch str {
-		case "+":
-			mem[pc] += 1
-		case "-":
-			mem[pc] -= 1
-		case "<":
-			pc += 1
-		case ">":
-			pc -= 1
-		case "[":
-		case "]":
-		case ",":
-		case ".":
-			fmt.Printf("%s", mem[pc])
-		default:
+		str := string(buff[:c])
+		for pc := 0; pc < c; pc++ {
+			switch string(str[pc]) {
+			case "+":
+				mem[reg] += 1
+			case "-":
+				mem[reg] -= 1
+			case "<":
+				reg += 1
+			case ">":
+				reg -= 1
+			case "[":
+				// if mem[reg] != 0 {
+				// 	break
+				// }
+				// nest := 0
+				// for pc < str.length {
+				// 	if string(str[pc]) == "[" {
+				// 		nest += 1
+				// 	} else if string(str[pc]) == "]" {
+				// 		nest -= 1
+				// 		if nest == 0 {
+				// 			break
+				// 		}
+				// 	}
+				// 	pc += 1
+				// }
+			case "]":
+				// if mem[reg] == 0 {
+				// 	break
+				// }
+				// nest := 0
+				// for {
+				// 	if reg <= 0 {
+				// 		break
+				// 	}
+				// 	if string(str[reg]) == "]" {
+				// 		nest += 1
+				// 	} else if string(str[reg]) == "[" {
+				// 		nest -= 1
+				// 		if nest == 0 {
+				// 			break
+				// 		}
+				// 	}
+				// 	reg -= 1
+				// }
+			case ",":
+			case ".":
+				fmt.Println(string(mem[reg]))
+			default:
+			}
 		}
 	}
 	input.Close()
